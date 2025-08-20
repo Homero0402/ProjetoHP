58ac9a9dc67a6147a291a365a4f82b6b6d357bc5
Homero0402 <homerolinakis09@gmail.com>
2025-08-19 21:16:26 -0300

    trabalhandom com operadores e variaveis

---
+++ exercicio.php
@@ -0,0 +1,13 @@
+<?php 
+
+	echo("Trabalhando com operadores em PHP <br />");
+	echo ("Operadores de soma <br />");
+	echo ("Operadores de subtração <br />");
+	echo ("Operadores de multiplicação <br />");
+	echo ("Operadores de divisão <br />");
+
+	echo("Insira a Operação desejada: <br/>");
+	
+
+
+ ?>
---
+++ variaveis.php
@@ -0,0 +1,5 @@
+<?php 
+	echo ("Trabalhando com variaveis em PHP <br/>");
+
+
+ ?>
